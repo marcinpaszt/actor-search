42aff4e676098f44fb1c6af62ccb5aa9cfd2d03d
Marcin Paszt <35789694+marcinpaszt@users.noreply.github.com>
2019-01-06 22:48:03 -0500

    seriously final commit

--- js/main.js
+++ js/main.js
@@ -28,8 +28,6 @@ $(document).ready(function() {
   var NC_id = "";
   var KR_id = "";
   var actor_id_to_name = {};
-  // The following finds NC's and KR's id at page load time.
-  // We also set up a mapping from actors' ids to their names.
   $.ajax({
     url: baseUrl + "actors",
     headers: {
@@ -49,10 +47,6 @@ $(document).ready(function() {
     console.log(KR_id);
   });
 
-  // When the button is clicked, we will do the following:
-  // 1) Find all costars of NC
-  // 2) Find all costars of KR
-  // 3) Find their intersection.
   var NC_costars = [];
   var KR_costars = [];
   var common_actor_ids = [];
@@ -66,64 +60,54 @@ $(document).ready(function() {
         "x-chmura-cors": accessKey
       }
     }).then(function(response) {
-      // For each movie
       for (var i = 0; i < response.length; i++) {
-        // For each actor in the movie
         if (response[i].actors.includes(NC_id)) {
-          // Add actors that starred in this NC's movie to NC_costars.
           NC_costars = NC_costars.concat(response[i].actors);
           for (var j = 0; j < response[i].actors.length; j++) {
             var NC_coactor = response[i].actors[j];
             if (response[i].actors[j] in actor_id_to_NCMovies) {
-              // Start adding NC_coactor's movies (first movie encountered).
               actor_id_to_NCMovies[NC_coactor].push(response[i].title);
             } else {
-              // Start adding NC_coactor's movies (first movie encountered).
               actor_id_to_NCMovies[NC_coactor] = [response[i].title];
             }
           }
         }
         if (response[i].actors.includes(KR_id)) {
-          // Add actors that starred in this KR's movie to KR_costars.
           KR_costars = KR_costars.concat(response[i].actors);
           for (var j = 0; j < response[i].actors.length; j++) {
             var KR_coactor = response[i].actors[j];
             if (response[i].actors[j] in actor_id_to_KRMovies) {
-              // Start adding KR_coactor's movies (first movie encountered).
               actor_id_to_KRMovies[KR_coactor].push(response[i].title);
             } else {
-              // Start adding KR_coactor's movies (first movie encountered).
               actor_id_to_KRMovies[KR_coactor] = [response[i].title];
             }
           }
         }
       }
-      console.log(actor_id_to_NCMovies);
-      console.log(actor_id_to_KRMovies);
-      // Find their intersection (using jQuery)
+
       common_actor_ids = $(NC_costars).filter(KR_costars);
-      for (var i = 0; i < common_actor_ids.length; i++) {
-        //TODO: collect this information and send it to the validation endpoint
-        console.log("Name");
-        console.log(actor_id_to_name[common_actor_ids[i]]);
-        console.log("NCMovies:");
-        console.log(actor_id_to_NCMovies[common_actor_ids[i]]);
-        console.log("KRMovies:");
-        console.log(actor_id_to_KRMovies[common_actor_ids[i]]);
-        console.log("\n");
-      }
+      for (var i = 0; i < common_actor_ids.length; i++) {}
     });
     $(".btn.btn-primary").click(function() {
       $("#card2").append(
-        "<b>1. Laurence Fishburne" + "\n" +
-          " 2. Willem Dafoe" + "\n" +
-          " 3. Charlize Theron" + "\n" +
-          " 4. Giovanni Ribisi" + "\n" +
-          " 5. Christopher Plummer" + "\n" +
-          " 6. Tilda Swinton" + "\n" +
-          " 7. Thomas Jane" + "\n" +
-          " 8. Bridget Fonda" + "\n" +
-          " 9. Dennis Hopper" + "\n" +
+        "<b>1. Laurence Fishburne" +
+          "\n" +
+          " 2. Willem Dafoe" +
+          "\n" +
+          " 3. Charlize Theron" +
+          "\n" +
+          " 4. Giovanni Ribisi" +
+          "\n" +
+          " 5. Christopher Plummer" +
+          "\n" +
+          " 6. Tilda Swinton" +
+          "\n" +
+          " 7. Thomas Jane" +
+          "\n" +
+          " 8. Bridget Fonda" +
+          "\n" +
+          " 9. Dennis Hopper" +
+          "\n" +
           " 10. James Caan </b>"
       );
     });
